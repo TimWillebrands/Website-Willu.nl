2c64e4299f2952cbe475ff6d7dd5a9c28dcc0573
Tim.Willebrands <Tim.Willebrands@gmail.com>
2013-09-09 00:16:00 +0200

    Popped stash

--- project/Build.scala
+++ project/Build.scala
@@ -7,8 +7,8 @@ import cloudbees.Plugin._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "Website"
-  val appVersion      = "0.2"
+  val appName         = "WilluWebsite"
+  val appVersion      = "0.3"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -24,8 +24,8 @@ object ApplicationBuild extends Build {
 	  cloudBeesSettings :_*).settings(
       CloudBees.applicationId := Some("willu/web"),
       CloudBees.username := Some("willu"),
-      CloudBees.apiKey := Some("DD8E513F1DEEE470"),
-      CloudBees.apiSecret := Some("ZBRQXZV/ED1OLC1GUF7R34LJJEDD0CNPRMMXZORAHZC="),
+      CloudBees.apiKey := Some("17C8AE233DCE0056"),
+      CloudBees.apiSecret := Some("YKW1LI1SHLZRQ7VJYQQAKXT/THLEKMORQSQKR1HX5TC="),
       CloudBees.host := "https://api-eu.cloudbees.com/api",
       CloudBees.deployParams := Map("runtime.java_version" -> "1.7"),
       CloudBees.openOnUpload := false).settings(
